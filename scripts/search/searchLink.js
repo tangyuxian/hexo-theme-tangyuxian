0d28b317be3e9691c71c20f0a43a75440da12d65
tangyuxian <2120252100@qq.com>
2022-03-09 21:08:15 +0800

    修改jq的cdn地址

--- scripts/search/searchLink.js
+++ scripts/search/searchLink.js
@@ -1,6 +1,6 @@
 const js = hexo.extend.helper.get('js').bind(hexo)
 hexo.extend.injector.register('body_end', () => {
-  return js('https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.js');
+  return js('https://code.jquery.com/jquery-3.6.0.js');
 });
 hexo.extend.injector.register('body_end', () => {
   return js('/js/search.js');
